c15361d688c8059dc103f91982dd4efb506ac30c
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-10-30 20:08:09 +0300

    satart

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Header = () => {
+	return (
+		<>
+			<nav className='#e1bee7 purple lighten-4'>
+				<div  className="nav-wrapper">
+					<a href="!#" className="brand-logo right">Movies</a>
+					<ul id="nav-mobile" className="left hide-on-med-and-down">
+						<li><a href="!#">asend</a></li>
+						{/*<li><a href="badges.html">Components</a></li>*/}
+						{/*<li><a href="collapsible.html">JavaScript</a></li>*/}
+					</ul>
+				</div>
+			</nav>
+		</>
+	)
+}
+
