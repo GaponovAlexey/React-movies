e8835271f9e8903355b2e428aafb82409c642411
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-10-31 04:48:45 +0200

    save

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -4,12 +4,12 @@ export const Footer = () => {
 	return (
 
 		<footer className="page-footer yellow lighten-5" >
-				<div className="footer-copyright">
+			<div className="footer-copyright">
 				<div className="container" style={ { color: 'black' } } >
-						{new Date().toLocaleDateString() } <br></br>Hello from React
-					<a className="grey-text text-lighten-4 right" href="#!" style={ { color: 'black' } } >Repo</a>
-					</div>
+					{ new Date().toLocaleDateString() } <br></br>Hello from React
+					<a className="grey-text black text-lighten-4 right" href="https://github.com/kekelberi/demo">Repo</a>
 				</div>
-			</footer>
+			</div>
+		</footer>
 	)
 }
