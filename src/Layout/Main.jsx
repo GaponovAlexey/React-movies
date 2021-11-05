f5d0955fd0f6d12e2d5dca58d04a61d5c095073a
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-11-05 21:15:58 +0300

    save

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,33 +1,35 @@
-import React, { useEffect, useState, TransformStreamDefaultController } from 'react'
+import React, {
+  useEffect,
+  useState,
+  TransformStreamDefaultController,
+} from 'react'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
 import { Movies } from '../components/Movies'
 import { Preloader } from '../components/Preloader'
 import { Search } from '../components/Search'
 import { fetchMovieActions } from '../redux/reduser'
+
 const API_KEY = process.env.REACT_APP_API_KEY
 
 export const Main = () => {
+  const { status, movies } = useSelector((state) => state.movi)
 
-	const { status, movies } = useSelector(state => state.movi)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchMovieActions())
+  }, [])
 
-	
-	const dispatch = useDispatch()
-	useEffect(() => {
-		dispatch(fetchMovieActions())
-	}, [])
-	
-	
-	if (status === 'loading') {
-		return <Preloader />
-	}
-	return (
-		<main className='conteiner content' >
-			<div>
-				<Search  />
-				<Movies movies={ movies } />
+  if (status === 'loading') {
+    return <Preloader />
+  }
 
-			</div>
-		</main>
-	)
+  return (
+    <main className='conteiner content'>
+      <div>
+        <Search />
+        <Movies movies={movies} />
+      </div>
+    </main>
+  )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,89 +1,86 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { searchMovies } from "../redux/reduser";
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { searchMovies } from '../redux/reduser'
 const API_KEY = process.env.REACT_APP_API_KEY
 
-
-
 export const Search = () => {
-	const [state, setState] = useState('');
-	const [type, setType] = useState("all");
-	const dispatch = useDispatch()
+  const [state, setState] = useState('')
+  const [type, setType] = useState('all')
 
+  const dispatch = useDispatch()
 
+  const handleFilter = (e) => {
+    setType((type) => (type = e.target.dataset.type))
+    dispatch(searchMovies({ str: state, type }))
+  }
 
-	const handleFilter = (e) => {
-		setType(type => type = e.target.dataset.type)
-		dispatch(searchMovies({ str: state, type }))
-	};
+  const send = (e) => {
+    if (e.key === 'Enter') {
+      dispatch(searchMovies({ str: state, type }))
+      setState('')
+    }
+  }
 
-	const send = (e) => {
-		if (e.key === "Enter") {
-			dispatch(searchMovies(({ str: state, type })))
-			setState('');
-		}
-	};
-
-	const sendBut = () => {
-		if (state.trim().length) {
-			dispatch(searchMovies({ str: state, type }))
-			setState("");
-		}
-	};
-	return (
-		<div>
-			<div className="row">
-				<div className="col s12">
-					<div style={ { display: "flex" } } className="input-field ">
-						<input
-							value={ state }
-							onKeyDown={ send }
-							onChange={ (e) => setState(e.target.value) }
-							placeholder="search"
-						/>
-						<button className="btn search-btn" onClick={ sendBut }>
-							send
-						</button>
-					</div>
-				</div>
-				<div>
-					<p>
-						<label>
-							<input
-								className="with-gap inp"
-								name="type"
-								type="radio"
-								data-type="all"
-								onChange={ handleFilter }
-								checked={ type === 'all' }
-							/>
-							<span>all</span>
-						</label>
-						<label>
-							<input
-								className="with-gap inp"
-								name="type"
-								type="radio"
-								data-type="movie"
-								onChange={ handleFilter }
-								checked={ type === 'movie' }
-							/>
-							<span>Movies only</span>
-						</label>
-						<label>
-							<input
-								className="with-gap inp"
-								name="type"
-								type="radio"
-								data-type="series"
-								onChange={ handleFilter }
-								checked={ type === 'series' }
-							/>
-							<span>Series only</span>
-						</label>
-					</p>
-				</div>
-			</div>
-		</div>
-	);
-};
+  const sendBut = () => {
+    if (state.trim().length) {
+      dispatch(searchMovies({ str: state, type }))
+      setState('')
+    }
+  }
+  return (
+    <div>
+      <div className='row'>
+        <div className='col s12'>
+          <div style={{ display: 'flex' }} className='input-field '>
+            <input
+              value={state}
+              onKeyDown={send}
+              onChange={(e) => setState(e.target.value)}
+              placeholder='search'
+            />
+            <button className='btn search-btn' onClick={sendBut}>
+              send
+            </button>
+          </div>
+        </div>
+        <div>
+          <p>
+            <label>
+              <input
+                className='with-gap inp'
+                name='type'
+                type='radio'
+                data-type='all'
+                onChange={handleFilter}
+                checked={type === 'all'}
+              />
+              <span>all</span>
+            </label>
+            <label>
+              <input
+                className='with-gap inp'
+                name='type'
+                type='radio'
+                data-type='movie'
+                onChange={handleFilter}
+                checked={type === 'movie'}
+              />
+              <span>Movies only</span>
+            </label>
+            <label>
+              <input
+                className='with-gap inp'
+                name='type'
+                type='radio'
+                data-type='series'
+                onChange={handleFilter}
+                checked={type === 'series'}
+              />
+              <span>Series only</span>
+            </label>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
